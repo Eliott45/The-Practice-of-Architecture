209691817b67ecb08bb8c727fc08d3b68eab0a89
Maksim <eliott_nu@mail.ru>
2024-03-30 14:52:45 +0900

    Create simple game state machine

---
+++ Assets/CodeBase/Infrastructure/GameStateMachine.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace CodeBase.Infrastructure
+{
+	public class GameStateMachine
+	{
+		private readonly Dictionary<Type, IState> _states;
+		private IState _activeState;
+
+		public GameStateMachine()
+		{
+			_states = new Dictionary<Type, IState>();
+		}
+
+		public void Enter<TState>() where TState : IState
+		{
+			_activeState?.Exit();
+			
+			var state = _states[typeof(TState)];
+			_activeState = state;
+			state.Enter();
+		}
+	}
+}
